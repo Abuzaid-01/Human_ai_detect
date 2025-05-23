aecdf8d9e6fde5c68349f1d217c135b4938b17cb
Abuzaid <abuzaid0205@gmail.com>
2025-05-23 16:32:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app()
+app = create_app()  # This creates the app variable that Gunicorn will use
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
